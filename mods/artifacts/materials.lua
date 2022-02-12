4be6674cc79ee0b1de4a621de42825df3027b660
Yad <97409381+OCRenkist@users.noreply.github.com>
2022-02-11 21:16:03 -0500

    MoS: Whitespace in `artifacts/materials.lua`

--- mods/artifacts/materials.lua
+++ mods/artifacts/materials.lua
@@ -9,9 +9,9 @@ minetest.register_node("artifacts:moon_glass", {
 	stack_max    = minimal.stack_max_bulky *4,
 	light_source = 5,
 	paramtype    = "light",
-	use_texture_alpha = "clip",
 	sounds       = nodes_nature.node_sound_glass_defaults(),
 	groups       = {cracky = 1,},
+	use_texture_alpha = "clip",
 })
 ----SUN STONE----
 -- bright glowing glass, with warming effect
@@ -23,16 +23,16 @@ minetest.register_node("artifacts:sun_stone", {
 	node_box    = {
 		type  = "fixed",
 		fixed = {
-			{-0.25, -0.5, -0.25, 0.25, -0.4375, 0.25}, -- NodeBox1
-			{-0.0625, -0.5, 0.25, 0.0625, -0.4375, 0.5}, -- NodeBox2
-			{-0.0625, -0.5, -0.5, 0.0625, -0.4375, -0.25}, -- NodeBox3
-			{0.25, -0.5, -0.0625, 0.5, -0.4375, 0.0625}, -- NodeBox4
-			{-0.5, -0.5, -0.0625, -0.25, -0.4375, 0.0625}, -- NodeBox5
-			{-0.1875, -0.4375, -0.1875, 0.1875, -0.375, 0.1875}, -- NodeBox6
-			{0.25, -0.5, -0.3125, 0.3125, -0.4375, -0.25}, -- NodeBox8
-			{0.25, -0.5, 0.25, 0.3125, -0.4375, 0.3125}, -- NodeBox9
-			{-0.3125, -0.5, 0.25, -0.25, -0.4375, 0.3125}, -- NodeBox10
-			{-0.3125, -0.5, -0.3125, -0.25, -0.4375, -0.25}, -- NodeBox11
+			{-0.2500, -0.5000, -0.2500,  0.2500, -0.4375,  0.2500}, -- NodeBox1
+			{-0.0625, -0.5000,  0.2500,  0.0625, -0.4375,  0.5000}, -- NodeBox2
+			{-0.0625, -0.5000, -0.5000,  0.0625, -0.4375, -0.2500}, -- NodeBox3
+			{ 0.2500, -0.5000, -0.0625,  0.5000, -0.4375,  0.0625}, -- NodeBox4
+			{-0.5000, -0.5000, -0.0625, -0.2500, -0.4375,  0.0625}, -- NodeBox5
+			{-0.1875, -0.4375, -0.1875,  0.1875, -0.3750,  0.1875}, -- NodeBox6
+			{ 0.2500, -0.5000, -0.3125,  0.3125, -0.4375, -0.2500}, -- NodeBox8
+			{ 0.2500, -0.5000,  0.2500,  0.3125, -0.4375,  0.3125}, -- NodeBox9
+			{-0.3125, -0.5000,  0.2500, -0.2500, -0.4375,  0.3125}, -- NodeBox10
+			{-0.3125, -0.5000, -0.3125, -0.2500, -0.4375, -0.2500}, -- NodeBox11
 		}
 	},
 	light_source        = 13,
@@ -50,119 +50,119 @@ minetest.register_node("artifacts:sun_stone", {
 -- glowing glass, with cooling effect
 minetest.register_node("artifacts:moon_stone", {
 	description = "Moon Stone",
-	tiles = {"artifacts_moon_glass.png"},
-  stack_max = 1,
-	drawtype = "nodebox",
-	node_box = {
-		type = "fixed",
+	tiles       = {"artifacts_moon_glass.png"},
+	stack_max   = 1,
+	drawtype    = "nodebox",
+	node_box    = {
+		type  = "fixed",
 		fixed = {
-			{-0.0625, -0.5, -0.0625, 0.0625, -0.375, 0.0625}, -- NodeBox1
-			{0.0625, -0.5, 0.0625, 0.125, -0.375, 0.125}, -- NodeBox2
-			{0.0625, -0.5, -0.125, 0.125, -0.375, -0.0625}, -- NodeBox3
-			{-0.125, -0.5, -0.125, -0.0625, -0.375, -0.0625}, -- NodeBox4
-			{-0.125, -0.5, 0.0625, -0.0625, -0.375, 0.125}, -- NodeBox5
-			{0.125, -0.5, -0.125, 0.25, -0.4375, 0.125}, -- NodeBox6
-			{-0.25, -0.5, -0.125, -0.125, -0.4375, 0.125}, -- NodeBox7
-			{-0.125, -0.5, 0.125, 0.125, -0.4375, 0.25}, -- NodeBox8
-			{-0.125, -0.5, -0.25, 0.125, -0.4375, -0.125}, -- NodeBox9
-			{0.125, -0.5, -0.375, 0.25, -0.4375, -0.25}, -- NodeBox10
-			{0.25, -0.5, -0.25, 0.375, -0.4375, -0.125}, -- NodeBox11
-			{0.25, -0.5, 0.125, 0.375, -0.4375, 0.25}, -- NodeBox12
-			{0.125, -0.5, 0.25, 0.25, -0.4375, 0.375}, -- NodeBox13
-			{-0.25, -0.5, 0.25, -0.125, -0.4375, 0.375}, -- NodeBox14
-			{-0.375, -0.5, 0.125, -0.25, -0.4375, 0.25}, -- NodeBox15
-			{-0.375, -0.5, -0.25, -0.25, -0.4375, -0.125}, -- NodeBox16
-			{-0.25, -0.5, -0.375, -0.125, -0.4375, -0.25}, -- NodeBox17
-			{-0.125, -0.5, -0.5, 0.125, -0.4375, -0.375}, -- NodeBox18
-			{-0.125, -0.5, 0.375, 0.125, -0.4375, 0.5}, -- NodeBox19
-			{-0.5, -0.5, -0.125, -0.375, -0.4375, 0.125}, -- NodeBox20
-			{0.375, -0.5, -0.125, 0.5, -0.4375, 0.125}, -- NodeBox21
+			{-0.0625, -0.5, -0.0625,  0.0625, -0.3750,  0.0625}, -- NodeBox1
+			{ 0.0625, -0.5,  0.0625,  0.1250, -0.3750,  0.1250}, -- NodeBox2
+			{ 0.0625, -0.5, -0.1250,  0.1250, -0.3750, -0.0625}, -- NodeBox3
+			{-0.1250, -0.5, -0.1250, -0.0625, -0.3750, -0.0625}, -- NodeBox4
+			{-0.1250, -0.5,  0.0625, -0.0625, -0.3750,  0.1250}, -- NodeBox5
+			{ 0.1250, -0.5, -0.1250,  0.2500, -0.4375,  0.1250}, -- NodeBox6
+			{-0.2500, -0.5, -0.1250, -0.1250, -0.4375,  0.1250}, -- NodeBox7
+			{-0.1250, -0.5,  0.1250,  0.1250, -0.4375,  0.2500}, -- NodeBox8
+			{-0.1250, -0.5, -0.2500,  0.1250, -0.4375, -0.1250}, -- NodeBox9
+			{ 0.1250, -0.5, -0.3750,  0.2500, -0.4375, -0.2500}, -- NodeBox10
+			{ 0.2500, -0.5, -0.2500,  0.3750, -0.4375, -0.1250}, -- NodeBox11
+			{ 0.2500, -0.5,  0.1250,  0.3750, -0.4375,  0.2500}, -- NodeBox12
+			{ 0.1250, -0.5,  0.2500,  0.2500, -0.4375,  0.3750}, -- NodeBox13
+			{-0.2500, -0.5,  0.2500, -0.1250, -0.4375,  0.3750}, -- NodeBox14
+			{-0.3750, -0.5,  0.1250, -0.2500, -0.4375,  0.2500}, -- NodeBox15
+			{-0.3750, -0.5, -0.2500, -0.2500, -0.4375, -0.1250}, -- NodeBox16
+			{-0.2500, -0.5, -0.3750, -0.1250, -0.4375, -0.2500}, -- NodeBox17
+			{-0.1250, -0.5, -0.5000,  0.1250, -0.4375, -0.3750}, -- NodeBox18
+			{-0.1250, -0.5,  0.3750,  0.1250, -0.4375,  0.5000}, -- NodeBox19
+			{-0.5000, -0.5, -0.1250, -0.3750, -0.4375,  0.1250}, -- NodeBox20
+			{ 0.3750, -0.5, -0.1250,  0.5000, -0.4375,  0.1250}, -- NodeBox21
 		}
 	},
-  light_source = 7,
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	is_ground_content = false,
+	light_source        =  7,
+	paramtype           = "light",
+	paramtype2          = "wallmounted",
+	is_ground_content   = false,
 	sunlight_propagates = true,
-  use_texture_alpha = "blend",
-  temp_effect = -4,
-  temp_effect_max = 0,
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	groups = {cracky = 3, oddly_breakable_by_hand = 3, attached_node = 1, temp_effect = 1, temp_pass = 1},
+	use_texture_alpha   = "blend",
+	temp_effect         = -4,
+	temp_effect_max     =  0,
+	sounds              = nodes_nature.node_sound_glass_defaults(),
+	groups              = {
+		cracky = 3, 
+		oddly_breakable_by_hand = 3, 
+		attached_node = 1, 
+		temp_effect = 1, 
+		temp_pass = 1
+	},
 })
-
-------------------------------------
---STAR STONE
+----STAR STONE----
 --small glowing glass
-------------------------------------
-
 minetest.register_node("artifacts:star_stone", {
 	description = "Star Stone",
-	tiles = {"artifacts_moon_glass.png"},
-  stack_max = minimal.stack_max_medium * 2,
-	drawtype = "nodebox",
-	node_box = {
-		type = "fixed",
+	tiles       = {"artifacts_moon_glass.png"},
+	stack_max   = minimal.stack_max_medium * 2,
+	drawtype    = "nodebox",
+	node_box    = {
+		type  = "fixed",
 		fixed = {
-			{-0.0625, -0.5, -0.0625, 0.0625, -0.4375, 0.0625}, -- NodeBox1
-			{0.0625, -0.5, 0.0625, 0.125, -0.4375, 0.125}, -- NodeBox2
-			{0.0625, -0.5, -0.125, 0.125, -0.4375, -0.0625}, -- NodeBox3
-			{-0.125, -0.5, -0.125, -0.0625, -0.4375, -0.0625}, -- NodeBox4
-			{-0.125, -0.5, 0.0625, -0.0625, -0.4375, 0.125}, -- NodeBox5
+			{-0.0625, -0.5, -0.0625,  0.0625, -0.4375,  0.0625}, -- NodeBox1
+			{ 0.0625, -0.5,  0.0625,  0.1250, -0.4375,  0.1250}, -- NodeBox2
+			{ 0.0625, -0.5, -0.1250,  0.1250, -0.4375, -0.0625}, -- NodeBox3
+			{-0.1250, -0.5, -0.1250, -0.0625, -0.4375, -0.0625}, -- NodeBox4
+			{-0.1250, -0.5,  0.0625, -0.0625, -0.4375,  0.1250}, -- NodeBox5
 		}
 	},
-  light_source = 3,
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	use_texture_alpha = "clip",
+	light_source        = 3,
+	paramtype           = "light",
+	paramtype2          = "wallmounted",
+	use_texture_alpha   = "clip",
 	sunlight_propagates = true,
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	groups = {oddly_breakable_by_hand = 3, attached_node = 1, temp_pass = 1},
+	sounds              = nodes_nature.node_sound_glass_defaults(),
+	groups              = {oddly_breakable_by_hand = 3, attached_node = 1, temp_pass = 1},
 })
-------------------------------------
---ANTIQUORIUM
+----ANTIQUORIUM----
 --the super material of the ancients
-------------------------------------
 minetest.register_node("artifacts:antiquorium", {
 	description = "Antiquorium",
-	tiles = {"artifacts_antiquorium.png"},
-  stack_max = minimal.stack_max_bulky *4,
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	groups = {cracky = 1,},
+	tiles       = {"artifacts_antiquorium.png"},
+	stack_max   = minimal.stack_max_bulky *4,
+	sounds      = nodes_nature.node_sound_glass_defaults(),
+	groups      = {cracky = 1,},
 })
 ----ANTIQUORIUM LADDER----
 minetest.register_node("artifacts:antiquorium_ladder", {
-	description     = "Antiquorium Ladder",
-	drawtype        = "signlike",
-	stack_max       = minimal.stack_max_medium,
-	tiles           = {"artifacts_antiquorium_ladder.png"},
-	inventory_image = "artifacts_antiquorium_ladder.png",
-	wield_image     = "artifacts_antiquorium_ladder.png",
-	paramtype       = "light",
-	paramtype2      = "wallmounted",
+	description       = "Antiquorium Ladder",
+	drawtype          = "signlike",
+	stack_max         = minimal.stack_max_medium,
+	tiles             = {"artifacts_antiquorium_ladder.png"},
+	inventory_image   = "artifacts_antiquorium_ladder.png",
+	wield_image       = "artifacts_antiquorium_ladder.png",
+	paramtype         = "light",
+	paramtype2        = "wallmounted",
 	sunlight_propagates = true,
-	walkable  = false,
-	climbable = true,
+	walkable          = false,
+	climbable         = true,
 	is_ground_content = false,
-	selection_box = {
+	sounds            = nodes_nature.node_sound_glass_defaults(),
+	groups            = {cracky = 2},
+	selection_box     = {
 		type = "wallmounted",
 		--wall_top == <default>
 		--wall_bottom == <default>
 		--wall_side == <default>
 	},
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	groups = {cracky = 2},
 })
 ----ANTIQUORIUM DOOR----
 doors.register("door_antiquorium", {
-		tiles           = {{
-			name = "artifacts_antiquorium_door.png", 
-			backface_culling = true
-		}},
-		description     = "Antiquorium Door",
-		inventory_image = "artifacts_antiquorium_door_item.png",
-		groups = {cracky = 2},
-		sounds = nodes_nature.node_sound_glass_defaults(),
+	description      = "Antiquorium Door",
+	inventory_image  = "artifacts_antiquorium_door_item.png",
+	groups           = {cracky = 2},
+	sounds           = nodes_nature.node_sound_glass_defaults(),
+	tiles            = {{
+		name             = "artifacts_antiquorium_door.png", 
+		backface_culling = true
+	}},
 })
 doors.register_trapdoor("artifacts:trapdoor_antiquorium", {
 	description     = "Antiquorium Trapdoor",
@@ -181,20 +181,20 @@ minetest.register_node('artifacts:trampoline', {
 	drawtype    = "nodebox",
 	paramtype   = "light",
 	paramtype2  = "wallmounted",
+	groups      = {fall_damage_add_percent = -70, bouncy = 85, oddly_breakable_by_hand = 3, temp_pass = 1},
+	sounds      = nodes_nature.node_sound_defaults({footstep = {name = "artifacts_bouncy", gain = 0.8}}),
 	node_box    = {
 		type  = "fixed",
 		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, -0.3125, 0.5}, -- NodeBox1
-			{-0.5, 0.3125, -0.5, 0.5, 0.5, 0.5}, -- NodeBox2
+			{-0.5000, -0.5000, -0.5000, 0.5000, -0.3125, 0.5000}, -- NodeBox1
+			{-0.5000,  0.3125, -0.5000, 0.5000,  0.5000, 0.5000}, -- NodeBox2
 			{-0.1875, -0.3125, -0.1875, 0.1875, -0.1875, 0.1875}, -- NodeBox3
-			{-0.1875, 0.1875, -0.1875, 0.1875, 0.3125, 0.1875}, -- NodeBox4
-			{-0.125, 0.0625, -0.125, 0.125, 0.1875, 0.125}, -- NodeBox5
-			{-0.125, -0.1875, -0.125, 0.125, -0.0625, 0.125}, -- NodeBox6
-			{-0.1875, -0.0625, -0.1875, 0.1875, 0.0625001, 0.1875}, -- NodeBox7
+			{-0.1875,  0.1875, -0.1875, 0.1875,  0.3125, 0.1875}, -- NodeBox4
+			{-0.1250,  0.0625, -0.1250, 0.1250,  0.1875, 0.1250}, -- NodeBox5
+			{-0.1250, -0.1875, -0.1250, 0.1250, -0.0625, 0.1250}, -- NodeBox6
+			{-0.1875, -0.0625, -0.1875, 0.1875,  0.0625, 0.1875}, -- NodeBox7
 		}
 	},
-	groups = {fall_damage_add_percent=-70, bouncy = 85, oddly_breakable_by_hand = 3, temp_pass = 1},
-	sounds = nodes_nature.node_sound_defaults({footstep = {name="artifacts_bouncy", gain = 0.8}}),
 })
 ----TRAMPOLINE----
 minetest.register_node('artifacts:conveyor', {
@@ -204,12 +204,12 @@ minetest.register_node('artifacts:conveyor', {
 	drawtype     = "nodebox",
 	light_source = 2,
 	paramtype    = "light",
+	groups       = {slippery = 1000, oddly_breakable_by_hand = 3, temp_pass = 1},
+	sounds       = nodes_nature.node_sound_defaults({footstep = {name="artifacts_transport_fail", gain = 0.5}}),
 	node_box     = {
 		type  = "fixed",
 		fixed = {
 			{-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}, -- NodeBox1
 		}
 	},
-	groups       = {slippery = 1000, oddly_breakable_by_hand = 3, temp_pass = 1},
-	sounds       = nodes_nature.node_sound_defaults({footstep = {name="artifacts_transport_fail", gain = 0.5}}),
 })
