c1f57a5f67e03912ac1bfe92b0d58baf77cb5e02
Yad <97409381+OCRenkist@users.noreply.github.com>
2022-02-12 23:56:13 -0500

    MoS: Whitespace in `crafts.lua`

--- mods/animals/crafts.lua
+++ mods/animals/crafts.lua
@@ -14,26 +14,26 @@ local random = math.random
 local floor  = math.floor
 ----Carcasses----
 local box_small_invert = {
-	{-0.1250, -0.5000, -0.1875, 0.1250, -0.4375,  0.1875}, -- NodeBox1
-	{-0.0625, -0.4375, -0.1875, 0.0625, -0.3750,  0.1875}, -- NodeBox2
-	{-0.0625, -0.5000, -0.2500, 0.0625, -0.4375, -0.1875}, -- NodeBox3
-	{-0.0625, -0.5000,  0.1875, 0.0625, -0.4375,  0.2500}, -- NodeBox4
+	{-0.1250, -0.5000, -0.1875,  0.1250, -0.4375,  0.1875}, -- NodeBox1
+	{-0.0625, -0.4375, -0.1875,  0.0625, -0.3750,  0.1875}, -- NodeBox2
+	{-0.0625, -0.5000, -0.2500,  0.0625, -0.4375, -0.1875}, -- NodeBox3
+	{-0.0625, -0.5000,  0.1875,  0.0625, -0.4375,  0.2500}, -- NodeBox4
 }
 local box_large_invert = {
-	{-0.1875, -0.500, -0.250, 0.1875, -0.3750,  0.250}, -- NodeBox1
-	{-0.1250, -0.375, -0.250, 0.1250, -0.3125,  0.250}, -- NodeBox2
-	{-0.0625, -0.500, -0.375, 0.0625, -0.3750, -0.250}, -- NodeBox3
-	{-0.0625, -0.500,  0.250, 0.0625, -0.3750,  0.375}, -- NodeBox4
+	{-0.1875, -0.5000, -0.2500,  0.1875, -0.3750,  0.2500}, -- NodeBox1
+	{-0.1250, -0.3750, -0.2500,  0.1250, -0.3125,  0.2500}, -- NodeBox2
+	{-0.0625, -0.5000, -0.3750,  0.0625, -0.3750, -0.2500}, -- NodeBox3
+	{-0.0625, -0.5000,  0.2500,  0.0625, -0.3750,  0.3750}, -- NodeBox4
 }
 local box_small_bird = {
-	{-0.1250, -0.500, -0.1875,  0.1250, -0.3750,  0.1250}, -- NodeBox1
-	{-0.0625, -0.375, -0.1875,  0.0625, -0.3125,  0.0625}, -- NodeBox2
-	{-0.0625, -0.500, -0.3125,  0.0625, -0.3750, -0.1875}, -- NodeBox3
-	{-0.0625, -0.500,  0.1250,  0.0625, -0.4375,  0.2500}, -- NodeBox4
-	{ 0.1250, -0.500, -0.1250,  0.3125, -0.4375,  0.0000}, -- NodeBox5
-	{-0.3125, -0.500, -0.1250, -0.1250, -0.4375,  0.0000}, -- NodeBox6
-	{ 0.1250, -0.500,  0.0625,  0.1875, -0.4375,  0.2500}, -- NodeBox7
-	{-0.1875, -0.500,  0.0625, -0.1250, -0.4375,  0.2500}, -- NodeBox8
+	{-0.1250, -0.5000, -0.1875,  0.1250, -0.3750,  0.1250}, -- NodeBox1
+	{-0.0625, -0.3750, -0.1875,  0.0625, -0.3125,  0.0625}, -- NodeBox2
+	{-0.0625, -0.5000, -0.3125,  0.0625, -0.3750, -0.1875}, -- NodeBox3
+	{-0.0625, -0.5000,  0.1250,  0.0625, -0.4375,  0.2500}, -- NodeBox4
+	{ 0.1250, -0.5000, -0.1250,  0.3125, -0.4375,  0.0000}, -- NodeBox5
+	{-0.3125, -0.5000, -0.1250, -0.1250, -0.4375,  0.0000}, -- NodeBox6
+	{ 0.1250, -0.5000,  0.0625,  0.1875, -0.4375,  0.2500}, -- NodeBox7
+	{-0.1875, -0.5000,  0.0625, -0.1250, -0.4375,  0.2500}, -- NodeBox8
 }
 local box_small_fish = {
 	{-0.1250, -0.5000, -0.1875,  0.1250, -0.4375,  0.1875}, -- NodeBox1
@@ -66,43 +66,34 @@ for i in ipairs(list) do
 	local stack = list[i][4]
 	local heat  = list[i][5]
 	--raw
-	minetest.register_node("animals:carcass_"..name, {
-		description = desc.." Carcass",
+	minetest.register_node("animals:carcass_" .. name, {
+		description = desc .. " Carcass",
 		tiles       = {"animals_carcass.png"},
 		drawtype    = "nodebox",
 		paramtype   = "light",
-		node_box    = {
-			type  = "fixed",
-			fixed = box
-		},
+		node_box    = {type = "fixed", fixed = box},
 		stack_max   = stack / 2,
 		groups      = {snappy = 3, dig_immediate = 3, falling_node = 1, temp_pass = 1, heatable = heat},
 		sounds      = nodes_nature.node_sound_defaults(),
 	})
 	--cooked
-	minetest.register_node("animals:carcass_"..name.. "_cooked", {
-		description = "Cooked "..desc,
+	minetest.register_node("animals:carcass_" .. name .. "_cooked", {
+		description = "Cooked " .. desc,
 		tiles       = {"nodes_nature_silt.png"},
 		drawtype    = "nodebox",
 		paramtype   = "light",
-		node_box    = {
-			type  = "fixed",
-			fixed = box
-		},
+		node_box    = {type = "fixed", fixed = box},
 		stack_max   = stack,
 		groups      = {snappy = 3, dig_immediate = 3, falling_node = 1, temp_pass = 1},
 		sounds      = nodes_nature.node_sound_defaults(),
 	})
 	--burned
-	minetest.register_node("animals:carcass_"..name.. "_burned", {
-		description = "Burned "..desc,
+	minetest.register_node("animals:carcass_" .. name .. "_burned", {
+		description = "Burned " .. desc,
 		tiles       = {"animals_carcass_burned.png"},
 		drawtype    = "nodebox",
 		paramtype   = "light",
-		node_box    = {
-			type  = "fixed",
-			fixed = box
-		},
+		node_box    = {type = "fixed", fixed = box},
 		stack_max   = stack,
 		groups      = {snappy = 3, dig_immediate = 3, falling_node = 1, temp_pass = 1},
 		sounds      = nodes_nature.node_sound_defaults(),
